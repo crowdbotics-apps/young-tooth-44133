83566ad3be893dda99504d450d9581cc962ce5ba
Crowdbotics <team@crowdbotics.com>
2023-10-25 04:16:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "young-tooth-44133",
+  url: "https://young-tooth-44133.botics.co",
+  api: "https://young-tooth-44133.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
